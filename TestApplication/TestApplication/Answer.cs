ecb5bd059dc83c55e2eb6cfc1fc58e5d2312b1eb
HerrRegent <56793952+HerrRegent@users.noreply.github.com>
2019-10-20 17:15:38 +0300

    Add files via upload

---
+++ TestApplication/TestApplication/Answer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+    [System.Serializable]
+    public class Answer
+    {
+        public string Text { get; set; }
+        public bool IsCorrect { get; set; }
+        public bool ActualValue { get; set; }
+
+        public Answer()
+        {
+
+        }
+
+        public Answer(string Text, bool IsCorrect = false)
+        {
+            this.Text = Text;
+            this.IsCorrect = IsCorrect;
+        }
+    }
+}
---
+++ TestApplication/TestApplication/ViewModels/MainviewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace TestApplication.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Question> Questions
+        {
+            get;
+            set;
+        }
+
+        public MainViewModel()
+        {
+            Questions = new ObservableCollection<Question>(QuestionDataBase.GetQuestions());
+            Questions.CollectionChanged += (send, e) =>
+            {
+                PropertyChange(nameof(Questions));
+            };
+        }
+
+        public void PropertyChange(string propertyName)
+        {
+            PropertyChangedEventArgs args = new PropertyChangedEventArgs(propertyName);
+            PropertyChanged?.Invoke(this, args);
+        }
+    }
+}
