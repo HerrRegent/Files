820a0c15389bd5073d68fd528238cff640d4fca2
HerrRegent <56793952+HerrRegent@users.noreply.github.com>
2019-10-28 09:51:20 +0300

    Add files via upload

---
+++ TestApplication v2/TestApplication/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+    [System.Serializable]
+    public class Question
+    {
+        public enum QuestionType
+        {
+            Standart
+        }
+
+        public string Text { get; set; }
+        public QuestionType Type { get; set; }
+
+        public Answer[] Answers { get; set; }
+    }
+}
